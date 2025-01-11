2b482ebca55ab9fa49a0b16325ff7fd3d87369ee
juan <juanyemi2812@gmail.com>
2025-01-11 02:21:19 -1000

    agrege una carpeta a mi repositorio
    agrege la carpeta del proyecto de unity a mi repositorio

---
+++ Clase-Ebac/Assets/scrips/scripMensajes.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class scripMensajes : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Awake()
+    {
+        Debug.Log("hola mundo desde awake");
+
+    }
+
+    void Start()
+    {
+        Debug.LogError("mundo desde start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogWarning("hola mundo desde Update");
+    }
+
+
+    private void OnEnable()
+    {
+        Debug.Log("hola mundo desde onEnable");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogError("hola mundo desde OnDisable");
+    }
+}
